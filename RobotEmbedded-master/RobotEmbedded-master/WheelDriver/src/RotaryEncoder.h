f60ed9d37a5b49e8a6453ee0095c31cfd480bf3e
Chase Rony <chase@rony.com>
2019-04-30 14:48:49 -0400

    Fixed?

--- RobotEmbedded-master/RobotEmbedded-master/WheelDriver/src/RotaryEncoder.h
+++
@@ -1,9 +0,0 @@
-/*
- * RotaryEncoder.h
- *
- * Created: 4/18/2019 2:10:21 PM
- *  Author: colea
- */ 
-void RotarySetup();
-int getLeftRotaryCount();
-int getRightRotaryCount();
--- WheelDriver/src/main.c
+++ WheelDriver/src/main.c
@@ -7,7 +7,7 @@
 #include "MotorControls.h"
 #include "UltrasonicObservers.h"
 #include "RotaryEncoder.h"
-
+#include "IRsensor.h"
 
 
 int main (void)
@@ -17,105 +17,50 @@ int main (void)
 	setUpUltraSonic();
 	wheelSetup();
 	RotarySetup();
+	//IRsetup();
+	
 	stop();
 	//Serial0_config(115200, SERIAL_8N1);
-	setSpeed(99);
+	setSpeed(50);
+
+//Go Straight 8ft (open loop)
 	
-	while(1)
-	{
-		Serial0_config(115200, SERIAL_8N1);
-		char held[100];
-		sprintf(held, "Left: %d Right: %d", getLeftRotaryCount(), getRightRotaryCount());
-		Serial0_poll_print(held);
-		_delay_ms(5000);
-	}
-	while(1)
-	{
-		/*
-		goStraightDistance(200, 1);
-		_delay_ms(2000);
-		goStraightDistance(200, 0);
-		_delay_ms(2000);
-		*/
-	}
+// 	while(1)
+// 	{
+// 		goStraightDistance(244, 1);
+// 		stop();
+// 		_delay_ms(2000);
+// 	}
+
+
+
+//Tape tracking 
 	
-	while(1)
-	{
-		goStraightDistance(243, 1);
-		stop();
-		_delay_ms(1000);
-		TurnDegrees(180, 1);
-		stop();
-		_delay_ms(1000);
-		goStraightDistance(243, 1);
-		stop();
-		_delay_ms(1000);
-		TurnDegrees(180, 1);
-		stop();
-		_delay_ms(1000);
-	}
+// 	while(1)
+// 	{
+// 		IRrun();
+// 	}
+
+//Pivot Turns 
+
+// 	while(1)
+// 	{
+// 		TurnDegrees(90, 0);
+// 		stop();
+// 		_delay_ms(4000);
+// 		TurnDegrees(180,0);
+// 		stop();
+// 		_delay_ms(4000);
+// 		TurnDegrees(90,1);
+// 		stop();
+// 		_delay_ms(4000);
+// 		TurnDegrees(180,1);
+// 		stop();
+// 		_delay_ms(10000);
+// 	}	
 	
-	/*
-	while(1)
-	{
-		ultraSonicObservers();
-		_delay_ms(10);
-		int distanceLeft = getDistanceLeft();
-		int distanceRight = getDistanceRight();
-		
-		Serial0_config(115200, SERIAL_8N1);
-		char held[100];
-		sprintf(held, "Left: %d   Right: %d	", distanceLeft, distanceRight);
-		Serial0_poll_print(held);
-		_delay_ms(5000);
-		
-	}
-	*/
-	/*
-	while(1)
-	{
- 		TurnDegrees(45, 1);
-		stop();
- 		_delay_ms(4000);
- 		goBackwards();
-		goStraightDistance(20, 0);
- 		TurnDegrees(70, 0);
- 		stop();
-		 _delay_ms(4000);
- 		goBackwards();
- 		goStraightDistance(20, 0);
- 		TurnDegrees(70, 1);
- 		stop();
-		 _delay_ms(4000);
- 		goForwards();
- 		goStraightDistance(40, 1);
- 		stop();
-		 _delay_ms(4000);
- 		RadiusTurn(90, 20, 0);
- 		stop();
-		 _delay_ms(4000);
- 		RadiusTurn(90, 20, 1);
-		stop();
-		_delay_ms(4000);
-	}
-	*/
-	
-	/*
-	while(1)
-	{
-		setSpeed(99);
-		goStraightDistance(40);
-		_delay_ms(1000);
-		TurnDegrees(180, 0);
-		setSpeed(99); 
-		goStraightDistance(40);
-		_delay_ms(1000);
-		TurnDegrees(180, 1);
-		_delay_ms(1000);
-		setSpeed(99);
-	}
-	*/
-	
+
+//Automous Driving 
 	
 	while(1)
 	{
